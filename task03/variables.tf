09606f67a94dce7c52327eaf8c26d9ab38097d1b
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2024-08-20 16:14:36 +0300

    Update variables.tf

--- task03/variables.tf
+++ task03/variables.tf
@@ -1,50 +1,49 @@
 variable "resource_group_name" {
-  description = "Name of the Resource Group"
+  description = "Назва вже існуючої групи ресурсів"
   type        = string
   default     = "cmtr82c341d6-rg"
 }
 
 variable "location" {
-  description = "Azure region where resources will be created"
+  description = "Локація ресурсів"
   type        = string
   default     = "East US"
 }
 
 variable "storage_account_name" {
-  description = "Name of the Storage Account"
+  description = "Назва облікового запису сховища"
   type        = string
   default     = "cmtr82c341d6sa"
 }
 
 variable "vnet_name" {
-  description = "Name of the Virtual Network"
+  description = "Назва віртуальної мережі"
   type        = string
   default     = "cmtr82c341d6-vnet"
 }
 
-variable "subnet_names" {
-  description = "List of Subnet names"
-  type        = list(string)
-  default     = ["private", "public"]
-}
-
 variable "address_space" {
-  description = "The address space that is used by the virtual network"
+  description = "Адресний простір віртуальної мережі"
   type        = list(string)
   default     = ["10.0.0.0/16"]
 }
 
+variable "subnet_names" {
+  description = "Назви підмереж"
+  type        = list(string)
+  default     = ["private", "public"]
+}
+
 variable "subnet_prefixes" {
-  description = "List of address prefixes for the subnets"
+  description = "Адресні префікси підмереж"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "tags" {
-  description = "A map of tags to add to all resources"
+  description = "Теги для ресурсів"
   type        = map(string)
   default     = {
     environment = "production"
-    project     = "terraform-deployment"
   }
 }
