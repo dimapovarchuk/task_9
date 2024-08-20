188e73b13864c174371fd9091dd3df00e4b65e10
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2024-08-20 17:29:44 +0300

    Update variables.tf

--- task03/variables.tf
+++ task03/variables.tf
@@ -1,34 +1,43 @@
 variable "resource_group_name" {
   description = "The name of the existing resource group"
   type        = string
+  default     = "cmtr82c341d6-rg"
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account"
   type        = string
+  default     = "cmtr82c341d6sa"
 }
 
 variable "vnet_name" {
   description = "The name of the virtual network"
   type        = string
+  default     = "cmtr82c341d6-vnet"
 }
 
 variable "address_space" {
   description = "The address space for the virtual network"
   type        = list(string)
+  default     = ["10.0.0.0/16"]
 }
 
 variable "subnet_names" {
   description = "The names of the subnets"
   type        = list(string)
+  default     = ["private", "public"]
 }
 
 variable "subnet_prefixes" {
   description = "The address prefixes for the subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
+  default     = {
+    "environment" = "dev"
+  }
 }
