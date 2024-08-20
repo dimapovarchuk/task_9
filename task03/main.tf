f72d424e8aa292c58d0cc4e6a73652c953b18555
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2024-08-20 16:42:31 +0300

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -2,16 +2,16 @@ provider "azurerm" {
   features {}
 }
 
-# Отримуємо дані про вже існуючу групу ресурсів
-data "azurerm_resource_group" "rg" {
+# Використання даних про існуючу групу ресурсів
+data "azurerm_resource_group" "existing_rg" {
   name = var.resource_group_name
 }
 
 # Створення облікового запису сховища
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.existing_rg.name
+  location                 = data.azurerm_resource_group.existing_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags                     = var.tags
@@ -20,17 +20,17 @@ resource "azurerm_storage_account" "sa" {
 # Створення віртуальної мережі
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   address_space       = var.address_space
   tags                = var.tags
 }
 
-# Створення двох підмереж (private і public)
+# Створення підмереж
 resource "azurerm_subnet" "subnets" {
   count                = length(var.subnet_names)
   name                 = element(var.subnet_names, count.index)
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [element(var.subnet_prefixes, count.index)]
 }
