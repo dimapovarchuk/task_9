23b7f802a8ef42d772c9e3dd038a732ec5941925
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2024-08-20 16:42:56 +0300

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,19 +1,11 @@
-output "resource_group_name" {
-  description = "Назва групи ресурсів"
-  value       = data.azurerm_resource_group.rg.name
-}
-
 output "storage_account_name" {
-  description = "Назва облікового запису сховища"
-  value       = azurerm_storage_account.sa.name
+  value = azurerm_storage_account.sa.name
 }
 
 output "vnet_name" {
-  description = "Назва віртуальної мережі"
-  value       = azurerm_virtual_network.vnet.name
+  value = azurerm_virtual_network.vnet.name
 }
 
 output "subnet_names" {
-  description = "Назви підмереж"
-  value       = azurerm_subnet.subnets[*].name
+  value = [for s in azurerm_subnet.subnets : s.name]
 }
